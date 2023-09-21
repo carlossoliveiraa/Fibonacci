a22c5d4761932c0f12effbca10ad4297cba2c9a8
unknown <carlossoliveiraa@hotmail.com>
2023-09-21 10:11:31 -0300

    criacao csharp fibonacci

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int termo = 10; 
+        int resultado = FibonacciRecursivo(termo);
+
+        Console.WriteLine($"O {termo}-ésimo termo da sequência de Fibonacci é: {resultado}");
+    }
+
+    static int FibonacciRecursivo(int n)
+    {
+        if (n <= 1)
+            return n;
+        else
+            return FibonacciRecursivo(n - 1) + FibonacciRecursivo(n - 2);
+    }
+}
